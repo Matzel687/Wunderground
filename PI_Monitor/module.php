16b8a4eb6d2a62f2688ed968ea3e8d178ce5e8dc
Matzel687 <marcel.lanfermann@googlemail.com>
2016-01-31 20:02:14 +0100

    27

--- PI_Monitor/module.php
+++ PI_Monitor/module.php
@@ -17,8 +17,8 @@
 			$this->RegisterPropertyString("IPS_Pfad", "/usr/share/symcon");
 			$this->RegisterPropertyString("Netzwerkkarte", "eth0");
 			$this->RegisterPropertyInteger("UpdateInterval", 20);
-            $this->RegisterTimer("Update", 0, 'PI_Monitor_Update($_IPS[\'TARGET\']);');
-            
+            $this->RegisterTimer("Update", 0, "PI_Monitor_Update($_IPS['TARGET']);");
+
         }
  
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
