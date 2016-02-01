113ed2aa4f309d7f7aec454067e1f8f839f86a7e
Matzel687 <marcel.lanfermann@googlemail.com>
2016-02-01 16:50:14 +0100

    30

--- PI_Monitor/module.php
+++ PI_Monitor/module.php
@@ -1,138 +1,135 @@
-<?
+ <?
     // Klassendefinition
-    class PI_Monitor extends IPSModule {
-        // Der Konstruktor des Moduls
-        // Überschreibt den Standard Kontruktor von IPS
-        public function __construct($InstanceID) {
-            // Diese Zeile nicht löschen
-            parent::__construct($InstanceID);
- 
-            // Selbsterstellter Code
-        }
+class PI_Monitor extends IPSModule
+{
+   // Der Konstruktor des Moduls
+   // Überschreibt den Standard Kontruktor von IPS
+   public function __construct($InstanceID)
+		{
+         // Diese Zeile nicht löschen
+         parent::__construct($InstanceID);
+         // Selbsterstellter Code
+		}
 
-        public function Create() {
-            // Diese Zeile nicht löschen.
-            parent::Create();
-			
+   public function Create()
+		{
+     		 // Diese Zeile nicht löschen.
+    		  parent::Create();
+
 			$this->RegisterPropertyString("IPS_Pfad", "/usr/share/symcon");
 			$this->RegisterPropertyString("Netzwerkkarte", "eth0");
 			$this->RegisterPropertyInteger("UpdateInterval", 20);
-            $this->RegisterTimer("Update", 0, 'PIMonitor_Update($_IPS["TARGET"]);');
+			$this->RegisterTimer("Update", 0, 'PIMonitor_Update($_IPS["TARGET"]);');
+			$this->SetStatus(101); //Instanz wird erstellt!
+		}
 
-        }
- 
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
-        public function ApplyChanges() {
-            // Diese Zeile nicht löschen
-            parent::ApplyChanges();
-            
+   public function ApplyChanges()
+		{
+         // Diese Zeile nicht löschen
+         parent::ApplyChanges();
+
 			//Variablenprofil anlegen
 			if (!IPS_VariableProfileExists("Megabyte"))
-			{
-				IPS_CreateVariableProfile("Megabyte", 2);
-				IPS_SetVariableProfileValues("Megabyte", 0, 0, 2);
-				IPS_SetVariableProfileText("Megabyte",""," MB");
-			}
-            
+				{
+					IPS_CreateVariableProfile("Megabyte", 2);
+					IPS_SetVariableProfileValues("Megabyte", 0, 0, 2);
+					IPS_SetVariableProfileText("Megabyte",""," MB");
+				}
+
 			if (($this->ReadPropertyString("IPS_Pfad") != "") AND ($this->ReadPropertyString("Netzwerkkarte") != ""))
-			{
-            
-			//Variablen erstellen
-			$this->RegisterVariableFloat("CPU_idle","CPU-Auslastung","Humidity.F",1);
-			$this->RegisterVariableFloat("CPU_volts","CPU-Spannung","Volt",2);
-			$this->RegisterVariableFloat("CPU_temp","CPU-Temperatur","Temperature",3);
-			$this->RegisterVariableFloat("HDD_total","Gesamt Speicherplatz","Megabyte",4);
-			$this->RegisterVariableFloat("HDD_used","Belegter Speicherplatz","Megabyte",5);
-			$this->RegisterVariableFloat("HDD_percent","HDD-Belegung","Humidity.F",6);
-			$this->RegisterVariableFloat("HDD_syncom","IPS-Speicherbelegung","Megabyte",7);
-			$this->RegisterVariableFloat("RAM_total","Gesamt RAM","Megabyte",8);
-			$this->RegisterVariableFloat("RAM_used","Benutzer RAM","Megabyte",9);
-			$this->RegisterVariableFloat("RAM_percent","RAM-Auslastung","Humidity.F",10);
-			$this->RegisterVariableString("System_Info","System Informationen","HTMLBox",11);
-            
-		        //Timer erstellen
-		$this->SetTimerInterval("Update", $this->ReadPropertyInteger("UpdateInterval")*1000);
-
-		
-        
-			}
+				{
+					//Variablen erstellen
+					$this->RegisterVariableFloat("CPU_idle","CPU-Auslastung","Humidity.F",1);
+					$this->RegisterVariableFloat("CPU_volts","CPU-Spannung","Volt",2);
+					$this->RegisterVariableFloat("CPU_temp","CPU-Temperatur","Temperature",3);
+					$this->RegisterVariableFloat("HDD_total","Gesamt Speicherplatz","Megabyte",4);
+					$this->RegisterVariableFloat("HDD_used","Belegter Speicherplatz","Megabyte",5);
+					$this->RegisterVariableFloat("HDD_percent","HDD-Belegung","Humidity.F",6);
+					$this->RegisterVariableFloat("HDD_syncom","IPS-Speicherbelegung","Megabyte",7);
+					$this->RegisterVariableFloat("RAM_total","Gesamt RAM","Megabyte",8);
+					$this->RegisterVariableFloat("RAM_used","Benutzer RAM","Megabyte",9);
+					$this->RegisterVariableFloat("RAM_percent","RAM-Auslastung","Humidity.F",10);
+					$this->RegisterVariableString("System_Info","System Informationen","HTMLBox",11);
+		        //Timer zeit setzen
+					$this->SetTimerInterval("Update", $this->ReadPropertyInteger("UpdateInterval")*1000);
+					$this->SetStatus(102); //Instanz ist aktiv
+				}
 			else
-			{
-				//Instanz ist inaktiv
-				$this->SetStatus(104);
-			}
-        }		
-        
-        public function Update() {
-        $IPS_directory = $this->ReadPropertyString("IPS_Pfad");
-        $networkcard = $this->ReadPropertyString("Netzwerkkarte");
-        $CPU_idle =exec("mpstat| grep all| awk '{print $12}'"); // CPU Auslastung %idle
-        $CPU_temp = substr(exec('vcgencmd measure_temp'), 5, 4); //Temperatur CPU
-        $CPU_volts = substr(exec("vcgencmd measure_volts"),5,4); //CPU Spannung 
-        $RAM_total = exec("free -m| grep Mem | awk '{print $2}'"); //Freier RAM
-        $RAM_used = exec("free -m| grep Mem | awk '{print $3}'"); //Benutzer RAM 
-        $HDD_total = exec("df -m | grep /dev/root | awk '{print $2}'"); // Gesamt Speicherplatz SD-Karte
-        $HDD_used = exec("df -m | grep /dev/root | awk '{print $3}'"); // Belegter Speicherplatz SD-Karte
-        $HDD_percent = substr(exec("df -m | grep /dev/root | awk '{print $5}'"),-3,2); // Belegter Speicherplatz in % SD-Karte
-        $HDD_syncom = exec("du -sh -m $IPS_directory| awk '{print $1}'"); // Verzeichnissgröße IPS
-        $LAN_IP = substr(exec("/sbin/ifconfig $networkcard | grep 'inet Adresse'| awk '{print $2}'"),8); // IP Adresse
-        $Linux_Vers = exec('uname -snr'); // Linux Version
-        $SSH_Log = substr(exec("who -q | grep '#' | awk '{print $2}'"),6); // Anzahl SSH Verbindungen
-        $SSH_Connection = substr(exec("who -s"),14); // // SSH Verbindungen von Client xy
+				{
+					$this->SetStatus(104); //Instanz ist inaktiv
+				}
+		}
 
-$html = ' <table width="100%" border="0" cellpadding="0" cellspacing="2" align="center" valign="top" >
- <tr >
-<td align="center" valign="top"  width="100px"; rowspan="6">
-<img src="user/Raspi-PGB001.png" style="float:left"; width="110px">
- </tr>
- <tr>
-  <td align="left" valign="top">IP Adresse:</td>
-  <td align="right" valign="top">'.$LAN_IP.'</td>
- </tr>
- <tr>
-  <td align="left" valign="top">System Online seit:</td>
-  <td align="right" valign="top">'.$this->uptime().'</td>
- </tr>
- <tr>
-  <td align="left" valign="top">Linux Version:</td>
-  <td align="right" valign="top">'.$Linux_Vers.'</td>
- </tr>
-  <tr>
-  <td align="left" valign="top">SSH Verbindung:</td>
-  <td align="right" valign="top">'.$SSH_Connection.'</td>
- </tr>
-   <tr>
-  <td align="left" valign="top">IPS Version:</td>
-  <td align="right" valign="top">'.IPS_GetKernelVersion().'</td>
- </tr>
-</table>';
-
-SetValue($this->GetIDForIdent("CPU_idle"), 100 - $CPU_idle);
-SetValue($this->GetIDForIdent("CPU_volts"), $CPU_volts);
-SetValue($this->GetIDForIdent("CPU_temp"), $CPU_temp);
-SetValue($this->GetIDForIdent("HDD_total"), $HDD_total);
-SetValue($this->GetIDForIdent("HDD_used"), $HDD_used);
-SetValue($this->GetIDForIdent("HDD_percent"), $HDD_percent);
-SetValue($this->GetIDForIdent("HDD_syncom"), $HDD_syncom);
-SetValue($this->GetIDForIdent("RAM_total"), $RAM_total);
-SetValue($this->GetIDForIdent("RAM_used"), $RAM_used);
-SetValue($this->GetIDForIdent("RAM_percent"),($RAM_used/$RAM_total)*100);
-SetValue($this->GetIDForIdent("System_Info"), $html);
+   public function Update()
+		{
+				$IPS_directory = $this->ReadPropertyString("IPS_Pfad");
+				$networkcard = $this->ReadPropertyString("Netzwerkkarte");
+				$CPU_idle =exec("mpstat| grep all| awk '{print $12}'"); // CPU Auslastung %idle
+				$CPU_temp = substr(exec('vcgencmd measure_temp'), 5, 4); //Temperatur CPU
+				$CPU_volts = substr(exec("vcgencmd measure_volts"),5,4); //CPU Spannung
+				$RAM_total = exec("free -m| grep Mem | awk '{print $2}'"); //Freier RAM
+				$RAM_used = exec("free -m| grep Mem | awk '{print $3}'"); //Benutzer RAM
+				$HDD_total = exec("df -m | grep /dev/root | awk '{print $2}'"); // Gesamt Speicherplatz SD-Karte
+				$HDD_used = exec("df -m | grep /dev/root | awk '{print $3}'"); // Belegter Speicherplatz SD-Karte
+				$HDD_percent = substr(exec("df -m | grep /dev/root | awk '{print $5}'"),-3,2); // Belegter Speicherplatz in % SD-Karte
+				$HDD_syncom = exec("du -sh -m $IPS_directory| awk '{print $1}'"); // Verzeichnissgröße IPS
+				$LAN_IP = substr(exec("/sbin/ifconfig $networkcard | grep 'inet Adresse'| awk '{print $2}'"),8); // IP Adresse
+				$Linux_Vers = exec('uname -snr'); // Linux Version
+				$SSH_Log = substr(exec("who -q | grep '#' | awk '{print $2}'"),6); // Anzahl SSH Verbindungen
+				$SSH_Connection = substr(exec("who -s"),14); // // SSH Verbindungen von Client xy
+				$image = file_get_contents(__DIR__ . "/Raspi-PGB001.png")
+				$html = ' <table width="100%" border="0" cellpadding="0" cellspacing="2" align="center" valign="top" >
+							<<tr >
+							<td align="center" valign="top"  width="100px"; rowspan="6">
+							<img src="'.$image.'" style="float:left"; width="110px">
+							</tr>
+							<tr>
+							<td align="left" valign="top">IP Adresse:</td>
+							<td align="right" valign="top">'.$LAN_IP.'</td>
+							</tr>
+							<tr>
+							<td align="left" valign="top">System Online seit:</td>
+							<td align="right" valign="top">'.$this->uptime().'</td>
+							</tr>
+							<tr>
+							<td align="left" valign="top">Linux Version:</td>
+							<td align="right" valign="top">'.$Linux_Vers.'</td>
+							</tr>
+							<tr>
+							<td align="left" valign="top">SSH Verbindung:</td>
+							<td align="right" valign="top">'.$SSH_Connection.'</td>
+							</tr>
+							<tr>
+							<td align="left" valign="top">IPS Version:</td>
+							<td align="right" valign="top">'.IPS_GetKernelVersion().'</td>
+							</tr>
+							</table>';
 
 
+							SetValue($this->GetIDForIdent("CPU_idle"), 100 - $CPU_idle);
+							SetValue($this->GetIDForIdent("CPU_volts"), $CPU_volts);
+							SetValue($this->GetIDForIdent("CPU_temp"), $CPU_temp);
+							SetValue($this->GetIDForIdent("HDD_total"), $HDD_total);
+							SetValue($this->GetIDForIdent("HDD_used"), $HDD_used);
+							SetValue($this->GetIDForIdent("HDD_percent"), $HDD_percent);
+							SetValue($this->GetIDForIdent("HDD_syncom"), $HDD_syncom);
+							SetValue($this->GetIDForIdent("RAM_total"), $RAM_total);
+							SetValue($this->GetIDForIdent("RAM_used"), $RAM_used);
+							SetValue($this->GetIDForIdent("RAM_percent"),($RAM_used/$RAM_total)*100);
+							SetValue($this->GetIDForIdent("System_Info"), $html);
+		}
 
-        }
-        
-public function uptime()
-{
-$upSeconds = exec("/usr/bin/cut -d. -f1 /proc/uptime");
-$uptimeDays = floor($upSeconds /86400);
-$uptimeHours = $upSeconds /3600 % 24;
-$uptimeMin = $upSeconds /60 % 60;
-$uptime = " $uptimeDays Tag(e) $uptimeHours Stunde(n) $uptimeMin Minute(n)";
+	public function uptime()
+		{
+				$upSeconds = exec("/usr/bin/cut -d. -f1 /proc/uptime");
+				$uptimeDays = floor($upSeconds /86400);
+				$uptimeHours = $upSeconds /3600 % 24;
+				$uptimeMin = $upSeconds /60 % 60;
+				$uptime = " $uptimeDays Tag(e) $uptimeHours Stunde(n) $uptimeMin Minute(n)";
 
-return $uptime;
-}       
-    
-    }
+			return $uptime;
+		}
+
+}
 ?>
