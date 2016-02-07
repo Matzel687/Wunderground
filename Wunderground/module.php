66b14b6bbc4fc9a6558a78604358701a698086bb
Matzel687 <marcel.lanfermann@googlemail.com>
2016-02-07 17:38:03 +0100

    Wunderground

--- Wunderground/module.php
+++ Wunderground/module.php
@@ -228,9 +228,8 @@ private function VarLogging($VarName,$LogStatus,$Type)
 {
     $archiveHandlerID = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}')[0];
     AC_SetAggregationType($archiveHandlerID, $this->GetIDForIdent($VarName), $Type);
-    IPS_ApplyChanges($archiveHandlerID);
     AC_SetLoggingStatus($archiveHandlerID, $this->GetIDForIdent($VarName), $this->ReadPropertyBoolean($LogStatus));
-    IPS_ApplyChanges($archiveHandlerID);
+    //IPS_ApplyChanges($archiveHandlerID);
 }
 
 
