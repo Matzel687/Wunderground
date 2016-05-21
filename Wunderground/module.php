5e5ff26578f830d8fa10f2c075305a1e0355d628
Matzel687 <marcel.lanfermann@googlemail.com>
2016-05-21 11:52:26 +0200

    test

--- Wunderground/module.php
+++ Wunderground/module.php
@@ -187,7 +187,7 @@
     						}
             $jsonData = json_decode($GetData);
             for ($i=0; $i <4 ; $i++) { 
-             $data =  array($day => array(
+             $data =  array($i => array(
                 'Date' => $jsonData->forecast->simpleforecast->forecastday[$i]->date->epoch,
                 'text' => $jsonData->forecast->txt_forecast->forecastday[$i]->fcttext_metric,
                 'Icon'  => 'http://icons.wxug.com/i/c/k/'. $jsonData->forecast->simpleforecast->forecastday[$i]->icon.'.gif',
